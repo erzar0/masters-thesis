e6e7d3b67064007458358fbb552e73d51a378410
Eryk Zarębski <erykzarebski0@gmail.com>
2025-04-01 00:27:29 +0200

    Implement spectra calibration

--- src/Elements.py
+++ src/Elements.py
@@ -6,8 +6,8 @@ class Elements():
     LINES = {
          "cr":  (("k_alpha", 5.410, (1**2 + 0.5**2)**0.5), ("k_beta", 5.946, 0.15))
         , "as": (("k_alpha", 10.523, (1**2 + 0.51**2)**0.5), ("k_beta31", 11.723, (0.06**2 + 0.13**2)**0.5))
-        , "au": (("l_alpha2", 9.628, 0.11), ("l_alpha1", 9.713, 1), ("l_beta1", 11.442, 0.76), ("l_beta2", 11.584, 0.23), ("l_gamma", 13.381, 0.13))
-        , "ba": ()
+        , "au": (("l_alpha", 9.700, (1**2 + 0.11**2)**0.5), ("l_beta", 11.442, (0.76**2 + 0.23**2)**0.5), ("l_gamma", 13.381, 0.13))
+        , "ba": (("l_alpha", 4.460, (1**2 + 0.11**2)**0.5), ("l_beta", 4.82, 0.6))
         , "ca": (("k_alpha", 3.690, (1**2 + 0.5**2)**0.5), ("k_beta", 4.012, 0.13))
         , "cd": (("l_alpha", 3.130, (1**2 + 0.11**2)**0.5), ("l_beta1", 3.316, 0.58), ("l_beta215", 3.528, 0.15), ("l_gamma", 3.716, 0.06))
         , "co": (("k_alpha", 6.927, (1**2 + 0.51**2)**0.5), ("k_beta", 7.649, 0.17))
@@ -20,7 +20,7 @@ class Elements():
         , "sn": (("l_alpha", 3.444, (1**2 + 0.11**2)**0.5), ("l_beta", 3.662, 0.6), ("l_beta215", 3.904, 0.16), ("l_gamma", 4.131, 0.07))
         , "ti": (("k_alpha", 4.508, (1**2 + 0.5**2)**0.5), ("k_beta", 4.931, 0.15))
         , "zn": (("k_alpha", 8.637, (1**2 + 0.51**2)**0.5), ("k_beta", 9.572, 0.17))
-        , "zr": ()
+        , "zr": (("k_alpha", 15.740, (1**2 + 0.51**2)**0.5), )
 
         # , "ni": (("k_alpha", 7.470, (1**2 + 0.51**2)**0.5),    ("k_beta", 8.264, 0.17))
         # , "se": (("k_alpha", 11.200, (1**2 + 0.52**2)**0.5),  ("k_beta31", 12.492, (0.06**2 + 0.13**2)**0.5))
@@ -43,3 +43,7 @@ class Elements():
     @staticmethod
     def get_parsed_element_lines(element):
         return [{"mu": line[1], "sigma": Elements.calculate_sigma(line[1]), "intensity": line[2], "name": f"{element}_{line[0]}"} for line in Elements.LINES[element]]
+    
+    @staticmethod
+    def get_element_line_energy(symbol, line_name):
+        return [line[1] for line in Elements.LINES[symbol] if line[0] == line_name][0]
