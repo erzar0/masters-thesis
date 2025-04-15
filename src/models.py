00e18d15ca571b324cce2128e8426bf4935000eb
Eryk Zarębski <erykzarebski0@gmail.com>
2025-04-15 15:01:15 +0200

    Progress

--- src/models.py
+++ src/models.py
@@ -2,3 +2,55 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
+class VisionTransformer(nn.Module):
+    def __init__(self, input_size, patch_size, embed_dim, num_heads, num_classes, num_layers, hidden_dim, dropout_rate):
+        super(VisionTransformer, self).__init__()
+        num_patches = input_size // patch_size
+        self.patch_embed = nn.Conv1d(1, embed_dim, kernel_size=patch_size, stride=patch_size, bias=False)
+        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches, embed_dim))
+        self.dropout = nn.Dropout(dropout_rate)
+        self.transformer_blocks = nn.ModuleList([
+            TransformerBlock(embed_dim, num_heads, hidden_dim, dropout_rate) for _ in range(num_layers)
+        ])
+        self.norm = nn.LayerNorm(embed_dim)
+        self.fc = nn.Linear(embed_dim * num_patches, num_classes)
+        self.gelu = nn.GELU()
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.patch_embed(x)
+        x = x.flatten(2).transpose(1, 2)
+        x = self.dropout(x + self.pos_embed)
+        for i, block in enumerate(self.transformer_blocks):
+            x = block(x)
+        x = self.norm(x)
+        x = x.reshape(x.size(0), -1)
+
+        x = self.fc(x)
+        x = self.sigmoid(x)
+        return x
+
+class TransformerBlock(nn.Module):
+    def __init__(self, embed_dim, num_heads, hidden_dim, dropout_rate):
+        super(TransformerBlock, self).__init__()
+
+        self.norm1 = nn.LayerNorm(embed_dim)
+        self.attention = nn.MultiheadAttention(embed_dim, num_heads)
+        self.norm2 = nn.LayerNorm(embed_dim)
+        self.feedforward = nn.Sequential(
+            nn.Linear(embed_dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout_rate),
+            nn.Linear(hidden_dim, embed_dim),
+            nn.Dropout(dropout_rate)
+        )
+
+    def forward(self, x):
+        global attention_weights
+        attn_output, attn_weights = self.attention(*([self.norm1(x)] * 3), need_weights=True)
+        attention_weights = attn_weights
+        x = x + attn_output
+        x = x + self.feedforward(self.norm2(x))
+
+        return x
